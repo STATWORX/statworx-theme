76ffd77357d7129aeed956ff5dd4bc1cdbb1ab89
b-batton-sw <benedikt.batton@statworx.com>
2022-08-29 18:00:49 +0200

    ORG:ENH Add additional theme for altair and plotly as optional extras

--- src/statworx_theme/__init__.py
+++ src/statworx_theme/__init__.py
@@ -7,6 +7,11 @@ from .utils import (  # unimport:skip
     apply_style,
     register_blended_cmap,
     register_listed_cmap,
+    get_stwx_cmaps,
+    apply_style_altair,
+    apply_custom_colors_altair,
+    apply_style_plotly,
+    apply_custom_colors_plotly,
 )
 
 __version__ = "0.9.0"
