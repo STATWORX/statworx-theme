4afc9886c7ba863eb568e0040ce704b50eb992df
An Hoang <an.hoang@statworx.com>
2022-08-29 23:08:46 +0200

    [ORG:STYLE] Run linter on project

--- src/statworx_theme/__init__.py
+++ src/statworx_theme/__init__.py
@@ -4,14 +4,14 @@
 from .colormaps import *
 from .utils import (  # unimport:skip
     apply_custom_colors,
+    apply_custom_colors_altair,
+    apply_custom_colors_plotly,
     apply_style,
+    apply_style_altair,
+    apply_style_plotly,
+    get_stwx_cmaps,
     register_blended_cmap,
     register_listed_cmap,
-    get_stwx_cmaps,
-    apply_style_altair,
-    apply_custom_colors_altair,
-    apply_style_plotly,
-    apply_custom_colors_plotly,
 )
 
 __version__ = "0.10.0"
